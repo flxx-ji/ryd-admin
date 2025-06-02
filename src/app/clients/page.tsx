5e133c18689c9393941ddca82c381a53b6387d63
Jean-marc Bastareaud <jean-marcbastareaud@FlxxiBook.local>
2025-06-02 17:33:22 +0200

    ajout composant dans clients/reservations.page.tsx pour eviter le plantage de next

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,8 @@
+export default function ClientsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Liste des clients</h1>
+      <p>🚧 En cours de construction...</p>
+    </div>
+  );
+}
---
+++ src/app/reservations/page.tsx
@@ -0,0 +1,8 @@
+export default function ReservationsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Liste des réservations</h1>
+      <p>🚧 Cette page est en construction.</p>
+    </div>
+  );
+}
